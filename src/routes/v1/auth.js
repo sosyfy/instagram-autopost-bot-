7c1b7eb98370d9db370bb00865fb849feb3ad73e
Sospeter Karanja <98221088+sosyfy@users.noreply.github.com>
2022-12-10 18:42:54 +0300

    Initial commit

---
+++ src/routes/v1/auth.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const controller = require( '#controllers/auth')
+
+const router = express.Router()
+
+router
+    .route('/register')
+    .post(controller.signUp)
+
+router 
+    .route('/login')
+    .post(controller.logInWithEmailAndPassword)
+
+
+router 
+    .route('/logout')
+    .post(controller.logout)
+
+
+module.exports = router
